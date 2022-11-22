1742b079315683a9e91c94cb2be06650997d3107
aartitiwari <aarti.tiwarii96@gmail.com>
2022-11-22 22:04:38 +0530

    First route

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-console.log("testing");
-console.log("creds check")
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.get('/',(req,res)=>{
+    console.log("works");
+    res.send("works")
+})
+
+app.listen(PORT,()=>{
+    console.log(`express app listening on port  ${PORT}`);
+    
+
+  
+    app.get('/ram',(req,res)=>{
+        console.log("yeh aata kya");
+        res.send('ram called')
+    })
+})
